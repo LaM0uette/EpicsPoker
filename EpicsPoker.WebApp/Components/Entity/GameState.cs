62e979a51798d43a6aaf797b9a5f667065d516c3
LaM0uette <LaM0uette.dev@gmail.com>
2023-12-28 17:48:37 +0100

    setup chooses card

--- EpicsPoker.WebApp/Components/Entity/GameState.cs
+++ EpicsPoker.WebApp/Components/Entity/GameState.cs
@@ -4,4 +4,5 @@ public enum GameState
 {
     WaitingForPlayers,
     ChoosePics,
+    ChooseCards,
 }
--- EpicsPoker.WebApp/Components/Pages/RoomBase.cs
+++ EpicsPoker.WebApp/Components/Pages/RoomBase.cs
@@ -5,11 +5,38 @@ namespace EpicsPoker.WebApp.Components.Pages;
 
 public class RoomBase : ComponentBase
 {
+    #region Temporary
+
+    protected int PicsParameter { get; private set; } = 10;
+    protected int CurrentPics { get; private set; }
+
+    #endregion
+    
     #region Statements
 
     [Parameter] public string RoomName { get; set; } = "";
     
     protected GameState CurrentGameState = GameState.WaitingForPlayers;
+
+    #endregion
+
+    #region Events
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        CurrentGameState = GameState.ChoosePics;
+    }
+
+    protected void OnClickOnCard()
+    {
+        CurrentPics++;
+        
+        if (CurrentPics == PicsParameter)
+        {
+            CurrentGameState = GameState.ChooseCards;
+        }
+    }
 
     #endregion
 }
